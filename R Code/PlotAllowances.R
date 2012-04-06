99349625aac2fa7f2bcf23fde25bda5bda5c12fe
Michael <caewok_2000@yahoo.com>
2012-04-05 18:18:52 -0700

    Made a few improvements to the plots for the figures

--- R Code/PlotAllowances.R
+++ R Code/PlotAllowances.R
@@ -22,10 +22,12 @@ dat <- data.frame(
      )
 
 utilities_gg <- ggplot(dat, aes(as.factor(Year), Allowances, fill=Utility))
-utilities_gg + geom_bar(alpha=3/4, show_guide=F) + facet_wrap(~ Utility) + labs(x="Year", y="Allowances (MMT CO2e)")
+utilities_gg + geom_bar(alpha=3/4, show_guide=F) + facet_wrap(~ Utility) + 
+     labs(x="Year", y=expression(paste("Number of Allowances (", MMTCO[2], plain(e), ")"))) +
+     opts(theme_text(size=14), title="Utility Allowances")
 
 
-ggsave("Figures/Allowances for Utilities.jpg")
+ggsave("Figures/Allowances for Utilities.jpg", height=5, width=7, units="in")
 
 
 # ADVANCE AUCTION
@@ -34,10 +36,11 @@ dat <- data.frame(Auction_Year=2012:2020, Advance_Allowances=c(allowance_alloc.t
 dat$Vintage.Year <- c(2015:2020, NA, NA, NA)
 
 advance_gg <- ggplot(dat, aes(as.factor(Auction_Year), Advance_Allowances, fill=Vintage.Year))
-advance_gg + geom_bar(alpha=3/4) + labs(x="Auction Year", y="Advance Allowances (MMT CO2e)")
+advance_gg + geom_bar(alpha=3/4) + labs(x="Auction Year", y=expression(paste("Number of Allowances (", MMTCO[2], plain(e), ")"))) +
+     opts(theme_text(size=14), title="Advance Allowances")
 
 
-ggsave("Figures/Fig 3 - Advance Allowances.jpg")
+ggsave("Figures/Fig 3 - Advance Allowances.jpg", height=5, width=7, units="in")
 
 
 # PRICE FLOOR AND RESERVE PRICES
@@ -86,7 +89,7 @@ dat <- data.frame(Allowance.Amount=c(
                        rep(times=8, "Advance"),
                        rep(times=8, "IOU"),
                        rep(times=8, "POU"),
-                       rep(times=8, "Industry.Plus.Remainder")
+                       rep(times=8, "Industry +\nRemainder")
                        ),
                   Year=as.integer(rep(times=4, rownames(allowance_alloc.tbl)))
                   )
@@ -95,9 +98,10 @@ dat[dat$Allowance.Type == "Advance", "Year"] <- c(NA, NA, 2012:2017)
 dat <- na.omit(dat)
 
 auction_gg <- ggplot(dat, aes(Allowance.Type, weight=Allowance.Amount, fill=Allowance.Type))
-auction_gg + geom_bar(alpha=3/4) + facet_wrap(~ Year) + labs(y="Number of Allowances (MMT CO2e)") + opts(axis.text.x=theme_blank(), axis.title.x=theme_blank())
+auction_gg + geom_bar(alpha=3/4) + facet_wrap(~ Year) + labs(y="Number of Allowances (MMT CO2e)") + 
+     opts(axis.text.x=theme_blank(), axis.title.x=theme_blank(), theme_text(size=14), title="Potential Allowances for CA Cap-and-Trade Auction")
 
-ggsave("Figures/Fig 4 - Auction Allowances.jpg")
+ggsave("Figures/Fig 4 - Auction Allowances.jpg", height=5, width=7, units="in")
 
 
 
@@ -120,15 +124,16 @@ dat <- data.frame(Allowance.Amount=c(
                        rep(times=8, "VRE"),
                        rep(times=8, "POU"),
                        rep(times=8, "IOU"),
-                       rep(times=8, "Industry.Plus.Remainder")
+                       rep(times=8, "Industry +\nRemainder")
                        ),
                   Year=rep(times=6, rownames(allowance_alloc.tbl))
                   )
 
 allowance_gg <- ggplot(dat, aes(x=Allowance.Type, y=Allowance.Amount, fill=Allowance.Type))
-allowance_gg + geom_bar() + facet_wrap(~ Year) + opts(axis.text.x=theme_blank(), axis.title.x=theme_blank())
+allowance_gg + geom_bar() + facet_wrap(~ Year) + ylab(expression(paste("Number of Allowances (", MMTCO[2], plain(e), ")"))) +
+     opts(axis.text.x=theme_blank(), axis.title.x=theme_blank(), theme_text(size=14), title="Allowance Categories by Vintage Year")
 
-ggsave("Figures/Total Allowance Allocation.jpg")
+ggsave("Figures/Total Allowance Allocation.jpg", height=5, width=7, units="in")
 
 # TOTAL ALLOWANCE ALLOCATION ASSUMING CONSTANT INDUSTRY EMISSIONS FOR FREE ALLOWANCES
 # just like the previous figure, except industry allowances are estimated separately from remainder
